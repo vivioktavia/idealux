7351adb8744661278fbe4f8f2ece09a7b60bd836
Vivi Oktavia <vivi@ebizu.com>
2017-12-01 19:17:55 +0700

    store api url to environment

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Headers, Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class AuthService {
-  private BASE_URL: string = 'https://young-eyrie-51496.herokuapp.com';
+  private BASE_URL: string = environment.BASE_URL;
   private headers: Headers = new Headers({'Content-Type': 'application/json'});
   constructor(private http: Http) {}
   login(user): Promise<any> {
+    console.log(environment.BASE_URL);
     let url: string = `${this.BASE_URL}/api-token-auth/`;
     return this.http.post(url, user, {headers: this.headers}).toPromise();
   }
