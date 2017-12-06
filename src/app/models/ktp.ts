95b283a78e528ae183403d52e681a28c84f22cc0
Vivi Oktavia <vivi@ebizu.com>
2017-12-06 21:41:54 +0700

    add ktp model and ktp service

---
+++ src/app/models/ktp.ts
@@ -0,0 +1,13 @@
+export class Ktp {
+    constructor(    
+        public id: number,
+        public lotNo : string,
+        public descs : string,
+        public landArea : number,
+        public buildArea : number,
+        public rtNo : string,
+        public rwNo : string,
+        public blockNo : string,
+        public block : number,
+    ){}
+}
